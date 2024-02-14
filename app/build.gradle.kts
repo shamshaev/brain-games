bebf70ab1983fee9c345685f959685873ca7397d
shamshaev <an.shamshaev@gmail.com>
2024-02-14 23:06:27 +0300

    Remove test dependencies and related text

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,19 +11,6 @@ application {
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
 }
